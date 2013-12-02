8fd226a05b127ab1317275fe145e418fd5b2b660
Petik123 <picpetr@fel.cvut.cz>
2013-12-02 15:25:30 +0100

    pravidlo 1-10

--- hw2.m
+++ hw2.m
@@ -4,6 +4,7 @@
    Implementation of BOS state. 
    The state is represented by the list of pairs { varName, value }.
 *)
+ClearAll[oneStep];
 initState[] :=
     {}; 
 
@@ -16,18 +17,25 @@ get[state_, varName_String] :=
     If[ MemberQ[state, {varName, _}],
         Last[First[Cases[state, {varName, _}]]],
         Null
-    ]; 
+    ];
+intQ[i_]:=IntegerQ[i]
+doubleQ[d_]:=NumberQ[d] && Not[IntegerQ[d]]
 
 
 (*Big-Step Operational Semantics*)
+(*2*)oneStep[\[Sigma]_,CBlock[{s___}]]:=oneStep[\[Sigma],{s}];
 
-(*2*)oneStep[\[Sigma]_,CBlock[{s___}]]:={(* YOUR CODE HERE *)};
+(*3*)oneStep[\[Sigma]_,{}]:={\[Sigma],Null};
 
-(*3*)oneStep[\[Sigma]_,{}]:={(* YOUR CODE HERE *)};
+(*4*)oneStep[\[Sigma]_,{c_,p___}]:=oneStep[\[Sigma],p]&/@oneStep[\[Sigma],c];
 
-(*4*)oneStep[\[Sigma]_,{c_,p___}]:={(* YOUR CODE HERE *)};
+(*5*)oneStep[\[Sigma]_,CDeclare[type_,varName_String]]:={put[initState[],varName,Undefined],Null};
 
-(*5*)oneStep[\[Sigma]_,CDeclare[_,_]]:={(* YOUR CODE HERE *)};
+(*8*)oneStep[\[Sigma]_,k_]:={\[Sigma],k}
+
+(*9*)oneStep[\[Sigma]_,varName_String]:={\[Sigma],get[initState[],varName]}
+
+(*10*)oneStep[\[Sigma]_,CAssign[varName_String,e_]]:={put[initState[],varName,e],e};
 
 
 (*Typing System*)
@@ -41,3 +49,15 @@ get[state_, varName_String] :=
 (*42*)typeOf[\[CapitalGamma]_,CAssign[var_,e_]]:={(* YOUR CODE HERE *)};
 
 (*44*)typeOf[\[CapitalGamma]_,{a:CAssign[var_,e_],stm___}]:={(* YOUR CODE HERE *)};
+
+
+(*program=CBlock[{
+CDeclare["int","i"]
+}];
+oneStep[initState[],program]
+Reap[oneStep[initState[],program]]*)
+
+
+
+
+
